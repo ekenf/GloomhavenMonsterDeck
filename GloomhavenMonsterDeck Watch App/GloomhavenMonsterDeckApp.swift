ca64b28a91b8ccacfdf07e4193c04d5b405835a1
ekenf <38294319+ekenf@users.noreply.github.com>
2023-05-16 02:44:21 +0300

    Initial Commit

---
+++ GloomhavenMonsterDeck Watch App/GloomhavenMonsterDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloomhavenMonsterDeckApp.swift
+//  GloomhavenMonsterDeck Watch App
+//
+//  Created by Furkan Eken on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GloomhavenMonsterDeck_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
